9ddb85a3c7e20fcafce766566ffa35470b1bb62f
Luciana Sugeta <lulusugeta@gmail.com>
2021-03-09 13:37:54 -0500

    commit tree be5c54627253e7e582ab6e9ffedd5b036869f6c5

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,17 @@
-## code your solution here. 
+require 'pry'
+#Cat
+  #instantiates a new cat
+  #receives attr_accessor
+  #has a name
+  #is able to meow
+
+  class Cat
+    attr_accessor :name, :meow
+
+    def initialize
+        @meow
+         puts "meow!"
+    end
+  end
+
+  
